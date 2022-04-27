60c463f074b9f57bb840feb8f58006d00cc5857a
the_p <the_p@LAPTOP-77GCKEU2>
2022-04-27 12:02:07 +0200

    Segunda rama

--- src/entornos27/Prueba1.java
+++ src/entornos27/Prueba1.java
@@ -5,7 +5,7 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba para las ramas. " 
 				+ "Esta es la principal");
-
+		System.out.println("Este es un syso desde la segunda rama");
 	}
 
 }
