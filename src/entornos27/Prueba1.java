5b6b681dd4358652e786684aff72bb2ae9c14aed
the_p <the_p@LAPTOP-77GCKEU2>
2022-04-27 12:00:51 +0200

    Primera prueba

---
+++ src/entornos27/Prueba1.java
@@ -0,0 +1,11 @@
+package entornos27;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+		System.out.println("Esto es una prueba para las ramas. " 
+				+ "Esta es la principal");
+
+	}
+
+}
